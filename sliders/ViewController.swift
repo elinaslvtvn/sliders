4cd83d7aa1573e455069fc30e59042180e3e6291
vadardev <v.arduanov@blanc.ru>
2023-07-07 17:23:24 +0500

    set up color

--- sliders/ViewController.swift
+++ sliders/ViewController.swift
@@ -2,18 +2,50 @@
 //  ViewController.swift
 //  sliders
 //
-//  Created by Вадим Ардуанов on 07.07.2023.
+//  Created by Элина Ардуанова on 07.07.2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var blueValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var redValue: UILabel!
+    
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var redSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
+        
+        changeColor()
+        
+        
+        redValue.text = String(format: "%.2f", redSlider.value)
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+        
     }
-
-
+    
+    @IBAction func rgbCalculate(_ sender: UISlider) {
+        changeColor()
+        redValue.text = String(format: "%.2f", redSlider.value)
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
 }
 
